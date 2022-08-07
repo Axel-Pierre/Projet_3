9d4f1365e285482a371529eb405e1f8443947e7b
Axel-Pierre <72556861+Axel-Pierre@users.noreply.github.com>
2022-08-07 22:41:31 +0200

    mise en place de la barre de recherche

--- src/composants/Pokemon_list.jsx
+++ src/composants/Pokemon_list.jsx
@@ -9,58 +9,86 @@ import {useNavigate} from 'react-router-dom';
  function Pokemon_list(){
     
         
-    
-    
-   
    let  results = getPokemons();
    const url_data = [] ;
+   const [windowHeight, setWindowHeight] = useState();
+  const [scrollPosition, setScrollPosition] = useState();
    const [pokemons,setPokemons] = useState([]);
-
+   const [filtre, setFiltre] = useState([]);
    const [nextDatas,setNextData] = useState();
   
    //on recupere les resultats et on les stockes dans les states
-        if(nextDatas == ""){
+   useEffect(() =>{
         results.then( res =>{
         setPokemons(res.pokemons); 
         setNextData(res.next_data);
-        
-        
-        })}else{
-            results = getPokemons(nextDatas)
-            results.then( res =>{
-                setPokemons(pokemons.concat(res.pokemons)); 
-                setNextData(res.next_data);
-                
-      
-       
+        setFiltre(res.pokemons);
         })
-    }
+    },[])
 
+    function getScrollPosition(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const documentHeigth = Math.ceil(document.documentElement.scrollHeight);
+        const windowHeight = Math.ceil(window.innerHeight);
+        const scroll = Math.ceil(window.scrollY);
+        setWindowHeight(scroll);
+        setScrollPosition(documentHeigth - windowHeight);
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", getScrollPosition);
+        if (
+          scrollPosition !== undefined &&
+          nextDatas !== undefined &&
+          windowHeight !== undefined &&
+          windowHeight >= scrollPosition
+        ) {
+          getPokemons(nextDatas)
+            .then((response) => {
+              return response;
+            })
+            .then((res) => {
+              setNextData(res.data.next)
+                setPokemons((prevState)=>[...prevState, ...res.data.results])
+                setFiltre((prevState) => [...prevState,...res.data.results]);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+        return () => {
+          window.removeEventListener("scroll", getScrollPosition);
+        };
+      }, [windowHeight]);
       //ps je n'ai pas pu faire de state sur mon display suite à mon bug rencontré sur axios (voir le lisez-moi)
     const pokemon_array = [];
     const display = [];
 // du coup j'ai utiliser une alternative me permettant d'envoyer directement l'utilisateur sur la page du pokemon ciblé
 
-    const navigate = useNavigate();
+    
    let poke_search = "";
 
     pokemons.map((pokemon) => pokemon_array.push(pokemon.name))
     pokemons.map(pokemon =>  url_data.push(pokemon.url));
    
-    useEffect(()=>{
-       
-        const form = document.getElementById('form');
-        form.addEventListener('submit',(e)=>{
-            e.preventDefault();
+    
+        function  Search (){
+        
              poke_search = document.getElementById('poke_name').value;
-            navigate(`/display_pokemon:${poke_search}`);
-          
-    })})
+             if(poke_search !=null){
+             let  pokemon_filter  = filtre.filter(pokemon =>{ return pokemon.name.toLowerCase().includes(poke_search)})
+                setPokemons(pokemon_filter);
+             }
+             if(poke_search === ""){
+                setPokemons(filtre);
+             }
+            }
+      
     
 
-   for(let i=0; i<pokemon_array.length;i++){
-       display.push(
-            <Generate_pokemon_card name ={pokemon_array[i]} id= {i+1} key= {i+1}/>,
+   for(let i=0; i<pokemons.length;i++){
+       display?.push(
+            <Generate_pokemon_card name ={pokemons[i].name} id= {pokemons[i].url.split('/').reverse()[1]} key= {i+1}/>,
            
         )
    
@@ -70,7 +98,7 @@ import {useNavigate} from 'react-router-dom';
         <div>
             
               <form id="form" className={styles.searchform} >
-        <input  id='poke_name' type="text" placeholder="Pokemon Name or Pokemon ID"/>
+        <input onChange={Search} id='poke_name' type="text" placeholder="Pokemon Name"/>
         <button type='submit'>Search</button>
       </form>
             <div className={styles.pokemon_list}>
